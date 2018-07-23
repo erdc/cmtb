f5422324b28101433599730674392bebd8d5e8f1
Chuan Li <lic@oregonstate.edu>
2018-07-23 14:56:46 -0400

    Added optional arguments to dailyPlots.py and fixed a bug where error would be raised when data isn't avaible

--- plotting/dailyPlots.py
+++ plotting/dailyPlots.py
@@ -8,7 +8,7 @@ This makes observations vs model station plots for a user-specified number of da
 @organization: USACE CHL FRF
 
 """
-import sys, getopt, warnings, os, glob
+import sys, getopt, warnings, os, glob, argparse, shutil
 import numpy as np
 import datetime as DT
 import netCDF4 as nc
@@ -19,67 +19,89 @@ from testbedutils import sblib as sb
 from plotting import operationalPlots as oP
 
 # PARAMETERS
+modelList = ['STWAVE', 'CMS']
+prefixList = {'STWAVE': ['HP', 'FP', 'CB'],
+              'CMS': ['HP']}
 stationList = ['waverider-26m', 'waverider-17m', 'awac-11m', '8m-array', 
                'awac-6m', 'awac-4.5m', 'adop-3.5m', 'xp200m', 'xp150m', 'xp125m']
 fieldVarList = ['waveHs', 'xRadGrad', 'yRadGrad', 'dissipation']
-# fieldVarList = ['waveHs']
+numDays = 7
+workDir = '/home/frfuser/Documents/Figures/cmtb'
 angadj = 70
 logo_path = '../ArchiveFolder/CHL_logo.png'
 
 # MAIN CODE
 def main():
-
-    startTime, endTime, model, prefix, workingDirectory = getUsrInp()
+    startTime, endTime, modelList, prefixList, workDir = getUsrInp()
     datestring = (startTime.strftime('%Y-%m-%dT%H%M%SZ') + '_' +
                   endTime.strftime('%Y-%m-%dT%H%M%SZ'))
-    fpath = os.path.join(workingDirectory, model, prefix, datestring)
-    if not os.path.exists(fpath):
-        os.makedirs(fpath)
-    # Do stations first
-    go = getDataFRF.getObs(startTime, endTime)
-    gm = getDataFRF.getDataTestBed(startTime, endTime)
-    for station in stationList:
-        (time, obsStats, modStats, 
-         plotList, obsi, modi) = getStats(startTime, endTime, model, prefix, 
-                                          station, go, gm)
-        if time is None:
-            continue
-        for param in plotList:
-            obs = obsStats[param][obsi.astype(np.int)]
-            obs = checkMask(obs, 'observation')
-            mod = modStats[param][modi.astype(np.int)]
-            mod = checkMask(mod, 'model')
-            ofname = os.path.join(fpath, 'CMTB-waveModels_{}_{}_station_{}_{}_{}.png'
-                                  .format(model, prefix, station, param, datestring))
-            makePlots(ofname, param, time, obs, mod)
-    # Now do field
-    gm = getDataFRF.getDataTestBed(startTime, endTime)
-    for isLocal in [True, False]:
-        bathy = gm.getStwaveField('bathymetry', prefix, isLocal, model=model)
-        for varName in fieldVarList:
-            try:
-                var = gm.getStwaveField(varName, prefix, isLocal, model=model)
-            except AssertionError as err:
-                continue
-            for key in var.keys(): # Check data for masks
-                var[key] = checkMask(var[key], key + ' (in ' + varName + ')')
-            fieldpacket = makeFieldpacket(varName, var, isLocal)
-            grid = 'Local' if isLocal else 'Regional'
-            kwargs = {}
-            if varName == 'waveHs':
-                waveDm = gm.getStwaveField('waveDm', prefix, isLocal, model=model)['waveDm']
-                kwargs['directions'] = waveDm
-            print('Generating field variable plots for ' + prefix + ' ' + 
-                  model + ' ' + grid + ' ' + varName)
-            oP.plotSpatialFieldData(bathy, fieldpacket, os.path.join(fpath, varName), 
-                                        isLocal, model=model, **kwargs)
-            imList = sorted(glob.glob(fpath + '/' + varName + '_*.png'))
-            print('Generating field variable gifs for ' + prefix + ' ' + model 
-                  + ' ' + grid + ' ' + varName)
-            ofname = os.path.join(fpath, 'CMTB-waveModels_{}_{}_{}_{}_{}.gif'
-                                  .format(model, prefix, grid, varName, datestring))
-            sb.makegif(imList, ofname)
-            [os.remove(ff) for ff in imList]
+    for model in modelList:
+        prefixes = prefixList[model]
+        for prefix in prefixes:
+            fpath = os.path.join(workDir, model, prefix)
+            if not os.path.exists(fpath):
+                os.makedirs(fpath)
+            else:
+                files = glob.glob(os.path.join(fpath, '*'))
+                for f in files:
+                    if os.path.isfile(f): os.remove(f)
+                    if os.path.isdir(f): shutil.rmtree(f)
+            # Do stations first
+            go = getDataFRF.getObs(startTime, endTime)
+            gm = getDataFRF.getDataTestBed(startTime, endTime)
+            for station in stationList:
+                (time, obsStats, modStats, 
+                plotList, obsi, modi) = getStats(startTime, endTime, model, prefix, 
+                                                station, go, gm)
+                if time is None:
+                    continue
+                for param in plotList:
+                    obs = obsStats[param][obsi.astype(np.int)]
+                    obs = checkMask(obs, 'observation')
+                    mod = modStats[param][modi.astype(np.int)]
+                    mod = checkMask(mod, 'model')
+                    ofname = os.path.join(fpath, 'CMTB-waveModels_{}_{}_station_{}_{}_{}.png'
+                                        .format(model, prefix, station, param, datestring))
+                    makePlots(ofname, param, time, obs, mod)
+            # Now do field
+            gm = getDataFRF.getDataTestBed(startTime, endTime)
+            for isLocal in [True, False]:
+                try:
+                    bathy = gm.getModelField('bathymetry', prefix, isLocal, model=model)
+                except AssertionError as err:
+                    if err.message == " there's no data":
+                        local = ' local' if isLocal is True else ' regional'
+                        print('No ' + model + local + ' ' + prefix + ' field data for ' + datestring)
+                        continue
+                    else:
+                        raise err
+                for varName in fieldVarList:
+                    try:
+                        var = gm.getModelField(varName, prefix, isLocal, model=model)
+                    except AssertionError as err:
+                        if 'variable called is not in file please use' in err.message:
+                            continue
+                        else:
+                            raise err
+                    for key in var.keys(): # Check data for masks
+                        var[key] = checkMask(var[key], key + ' (in ' + varName + ')')
+                    fieldpacket = makeFieldpacket(varName, var, isLocal)
+                    grid = 'Local' if isLocal else 'Regional'
+                    kwargs = {}
+                    if varName == 'waveHs':
+                        waveDm = gm.getModelField('waveDm', prefix, isLocal, model=model)['waveDm']
+                        kwargs['directions'] = waveDm
+                    print('Generating field variable plots for ' + prefix + ' ' + 
+                        model + ' ' + grid + ' ' + varName)
+                    oP.plotSpatialFieldData(bathy, fieldpacket, os.path.join(fpath, varName), 
+                                                isLocal, model=model, **kwargs)
+                    imList = sorted(glob.glob(fpath + '/' + varName + '_*.png'))
+                    print('Generating field variable gifs for ' + prefix + ' ' + model 
+                        + ' ' + grid + ' ' + varName)
+                    ofname = os.path.join(fpath, 'CMTB-waveModels_{}_{}_{}_{}_{}.gif'
+                                        .format(model, prefix, grid, varName, datestring))
+                    sb.makegif(imList, ofname)
+                    [os.remove(ff) for ff in imList]
 
 # SUBROUTINES
 def makeFieldpacket(varName, var, isLocal):
@@ -111,43 +133,40 @@ def checkMask(var, varname):
         return var
 
 def getUsrInp():
-    inp = sys.argv[1:]
-    # Make sure arguments are passed correctly
-    if len(inp) != 4 and len(inp) == 0:
-        sys.exit('Error: four arguments are necessary. ' +
-                'Please refer to help (python dailyPlots.py help)')
-    # Help message
-    if inp[0] == 'help':
-        print('----------------------------------------------')
-        print('Usage is as follows:')
-        print('python dailyPlots.py endTime numDays model prefix workingDirectory')
-        print('----------------------------------------------')
-        print('endTime is the ending datetime in the format like 2018-04-01T15:00:00Z')
-        print('numDays is the number of days (integer) prior to endTime to use')
-        print('model is one of: STWAVE, CMS')
-        print('prefix is one of: HP, FP, CB')
-        print('workingDirectory is the path where the plots will be saved')
-        sys.exit()
-    # End and start times
-    try:
-        endTime = DT.datetime.strptime(inp[0], '%Y-%m-%dT%H:%M:%SZ')
-    except ValueError:
-        sys.exit('Error: the first argument needs to be a datetime in the ' +
-                'format %Y-%m-%dT%H:%M:%SZ')
-    try:
-        numDays = int(inp[1])
-    except ValueError:
-        sys.exit('Error: the second argument needs to be an integer')
-    model = inp[2]
-    if model not in ['STWAVE', 'CMS']:
-        sys.exit('Error: the third argument needs to be one of: STWAVE, CMS')
-    prefix = inp[3]
-    if prefix not in ['HP', 'FP', 'CB']:
-        sys.exit('Error: the third argument needs to be one of: HP, FP')
-    startTime = endTime - DT.timedelta(days=numDays)
-    # Working directory
-    workingDirectory = inp[4]
-    return startTime, endTime, model, prefix, workingDirectory
+    parser = argparse.ArgumentParser()
+    parser.add_argument('endTime', type=str,
+                        help='ending datetime in the format like 2018-04-01T15:00:00Z')
+    parser.add_argument('-numDays', type=int,
+                        help='number of (integer) days to process prior to endTime')
+    parser.add_argument('-model', type=str,
+                        help='list of numerical model to process from, e.g. "[STWAVE, CMS]"')
+    parser.add_argument('-prefix', type=str,
+                        help=('dictionary of prefixes for the each numerical model, e.g.'
+                              '"' + "{'STWAVE': ['HP', 'FP', 'CB'], 'CMS': ['HP']}" + '"'))
+    parser.add_argument('-workDir', type=str,
+                        help='path where the plots will be saved')
+    args = parser.parse_args()
+    args.endTime = DT.datetime.strptime(args.endTime, '%Y-%m-%dT%H:%M:%SZ')
+    if args.numDays == None:
+        args.numDays= numDays
+    if args.model == None:
+        args.model = modelList
+    elif args.model[0] == '[' and args.model[-1] == ']':
+        args.model = eval(args.model)
+    else:
+        args.model = [args.model]
+    if args.prefix == None:
+        args.prefix = prefixList
+    else:
+        args.prefix = eval(args.prefix)
+    if args.workDir == None:
+        args.workDir = workDir
+    for model in args.model:
+        if model not in args.prefix.keys():
+            sys.exit('Error: ' + model + ' prefixes not provided')
+    startTime = args.endTime - DT.timedelta(days=numDays)
+    
+    return startTime, args.endTime, args.model, args.prefix, args.workDir
 
 def getStats(startTime, endTime, model, prefix, station, go, gm):
     # Get observation data
@@ -163,7 +182,7 @@ def getStats(startTime, endTime, model, prefix, station, go, gm):
         obsStats = sbwave.stats1D(wo['fspec'], wo['wavefreqbin'])
     # Get model results
     with warnings.catch_warnings(record=True) as w:
-        wm = gm.getWaveSpecSTWAVE(prefix, station, model=model)
+        wm = gm.getWaveSpecModel(prefix, station, model=model)
         if wm == None:
             return [None] * 6
         if w != []:
