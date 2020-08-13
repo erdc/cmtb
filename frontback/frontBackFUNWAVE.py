Fixed px and py computation, depending on grid type (1d or 2d)

frontback/frontBackFUNWAVE.py
@@ -88,7 +88,8 @@ def FunwaveSimSetup(startTime, rawWL, rawspec, bathy, inputDict):
     else:
         ybounds = [600,1100]
 
-    print("DEBUG GABY: ybounds =",ybounds)
+    print("\n\nDEBUG GABY: ybounds =",ybounds,"\n\n")
+
     _, gridDict = prepdata.prep_SwashBathy(bathy['xFRF'][0], bathy['yFRF'], bathy,ybounds)  # non-inclusive index for yBounds
 
     # _____________ begin writing files _________________________
@@ -97,11 +98,12 @@ def FunwaveSimSetup(startTime, rawWL, rawspec, bathy, inputDict):
     #  prep_FunwaveBathy
 
     [Nglob,Mglob] = gridDict['elevation'].shape
-    px = np.floor(gridDict['elevation'].shape[0]/100)  # or something to this effect
-    py = np.floor(gridDict['elevation'].shape[1]/100)  # or something to this effect
+    px = np.floor(Mglob / 150)
+    if grid.lower() == '1d':
+        py = 1
+    else:
+        py = np.floor(Nglob / 150)
     nprocessors = px * py  # now calculated on init
-    ## TODO: @gaby, you can change anything you need here, may save some of the write input requirements (eg px, py,
-    # maybe others)
 
 
     fio = funwaveIO(fileNameBase=date_str, path_prefix=path_prefix, version_prefix=version_prefix, WL=WLpacket['avgWL'],
@@ -114,7 +116,7 @@ def FunwaveSimSetup(startTime, rawWL, rawspec, bathy, inputDict):
     # above.  For this we'll have to modify the "prep" functions to do that.   I'm happy to help point you to where
     # you need to modify as needed.
 
-    ## write spectra
+    ## write spectra, depth, and station files
     if grid.lower() == '1d':
         fio.Write_1D_Bathy(dx,dy,gridDict['elevation'])
         fio.Write_1D_Spectra_File(wavepacket)